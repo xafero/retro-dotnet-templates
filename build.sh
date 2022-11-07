0f90daa745e3145a499d680718713f9b394b5d1f
Hubertus Sack <hubertussack@outlook.com>
2022-11-07 20:25:29 +0100

    Adding script

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+dotnet pack --configuration Release
+dotnet new --uninstall Retro.DotNet.Templates
+dotnet new --install nupkg/Retro.DotNet.Templates.0.0.1.nupkg
+
+rm -R test
+mkdir test
+cd test
+
+mkdir Alpha
+cd Alpha
+dotnet new console-retro
+cd ..
+
+mkdir Beta
+cd Beta
+dotnet new classlib-retro
+cd ..
+
+mkdir Gamma
+cd Gamma
+dotnet new worker-retro
+cd ..
+
+mkdir Delta
+cd Delta
+dotnet new webapi-retro
+cd ..
+
+mkdir Epsilon
+cd Epsilon
+dotnet new xunit-retro
+cd ..
+
+cd ..
