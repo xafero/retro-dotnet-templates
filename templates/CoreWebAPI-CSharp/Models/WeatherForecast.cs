00b90652915e0f3ca078adb5b5d5b096d37cdfa9
xafero <xafero@users.noreply.github.com>
2024-02-02 17:04:44 +0100

    Example model is now record apparently

--- templates/CoreWebAPI-CSharp/Models/WeatherForecast.cs
+++ templates/CoreWebAPI-CSharp/Models/WeatherForecast.cs
@@ -2,14 +2,8 @@ using System;
 
 namespace Example.Models
 {
-    public class WeatherForecast
+    public record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
     {
-        public DateTime Date { get; set; }
-
-        public int TemperatureC { get; set; }
-
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string Summary { get; set; }
     }
 }
